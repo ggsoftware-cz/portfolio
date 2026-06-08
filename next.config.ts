25a15321479f433c24590a2a176934f6c779820e
Martin Holý <115696258+MartinHoly00@users.noreply.github.com>
2026-06-08 18:55:16 +0200

    simp

--- next.config.ts
+++ next.config.ts
@@ -2,22 +2,14 @@ import createNextIntlPlugin from "next-intl/plugin";
 import type { NextConfig } from "next";
 
 const withNextIntl = createNextIntlPlugin("./i18n/request.ts");
-const isGitHubPages = process.env.GITHUB_PAGES === "true";
-const repositoryParts = process.env.GITHUB_REPOSITORY?.split("/") ?? [];
-const repositoryName =
-  repositoryParts.length === 2 ? repositoryParts[1] : undefined;
 
 const nextConfig: NextConfig = {
-  ...(isGitHubPages
-    ? {
-        output: "export",
-        ...(repositoryName ? { basePath: `/${repositoryName}` } : {}),
-      }
-    : {}),
+  output: "export",
+
   images: {
     dangerouslyAllowSVG: true,
     contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
-    ...(isGitHubPages ? { unoptimized: true } : {}),
+    unoptimized: true,
   },
 };
 
