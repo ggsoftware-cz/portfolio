cba1a99ba6660c307d78d0ab0c394acda7e8ccb5
Martin Holý <115696258+MartinHoly00@users.noreply.github.com>
2026-06-08 18:45:33 +0200

    basePath git add . :wq

--- next.config.ts
+++ next.config.ts
@@ -1,23 +1,25 @@
-import createNextIntlPlugin from 'next-intl/plugin';
-import type {NextConfig} from 'next';
+import createNextIntlPlugin from "next-intl/plugin";
+import type { NextConfig } from "next";
 
-const withNextIntl = createNextIntlPlugin('./i18n/request.ts');
-const isGitHubPages = process.env.GITHUB_PAGES === 'true';
-const repositoryParts = process.env.GITHUB_REPOSITORY?.split('/') ?? [];
-const repositoryName = repositoryParts.length === 2 ? repositoryParts[1] : undefined;
+const withNextIntl = createNextIntlPlugin("./i18n/request.ts");
+const isGitHubPages = process.env.GITHUB_PAGES === "true";
+const repositoryParts = process.env.GITHUB_REPOSITORY?.split("/") ?? [];
+const repositoryName =
+  repositoryParts.length === 2 ? repositoryParts[1] : undefined;
 
 const nextConfig: NextConfig = {
   ...(isGitHubPages
     ? {
-        output: 'export',
-        ...(repositoryName ? {basePath: `/${repositoryName}`} : {})
+        output: "export",
+        ...(repositoryName ? { basePath: `/${repositoryName}` } : {}),
       }
     : {}),
+  basePath: "/portfolio",
   images: {
     dangerouslyAllowSVG: true,
     contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
-    ...(isGitHubPages ? {unoptimized: true} : {})
-  }
+    ...(isGitHubPages ? { unoptimized: true } : {}),
+  },
 };
 
 export default withNextIntl(nextConfig);
